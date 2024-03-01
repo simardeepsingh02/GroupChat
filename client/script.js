f004bdbf5eb505e22fddd35d672b1819bd1ca6d0
simar30122000 <iamsimar12@gmail.com>
2024-03-01 15:28:31 +0530

    m vn

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 
-const socket = new WebSocket('ws://main--chatomegal.netlify.app');
+const socket = new WebSocket('ws://localhost:3000');
 
 socket.addEventListener('open', (event) => {
     console.log('WebSocket connection opened:', event);
